5bd8d1f9235e7cd1675197b70685c2d54b0d8631
kajal <kajalbaisakh123@gmail.com>
2022-10-02 11:47:02 +0530

    work

--- router/userRouter.js
+++ router/userRouter.js
@@ -76,12 +76,12 @@ router.post('/UserRegistation', [
             const no = await User.find().then((item)=>{
                 console.log("item",item)
                 item.map((id)=>{console.log("uuid",id.sequence_id,id.companyid,companyid)
-                if(companyid!==id.companyid){
-                    company_id=companyid
-                }else{
-                    return response.status(401).json({ errors: [{ msg: 'give unique id , id already exists' ,user:user}] });
-                }
-                if (id.sequence_id< 0){
+                // if(companyid!==id.companyid){
+                //     company_id=companyid
+                // }else{
+                //     return response.status(401).json({ errors: [{ msg: 'give unique id , id already exists' ,user:user}] });
+                // }
+                if (id.sequence_id> 0){
                     const str =  id.sequence_id + 1
                     const pad = "0000"
                     const answer =  str;
